d9bbaa8d83dc1ace4163e5b662efcc54b13ca920
halfields <hfields697@aol.com>
2017-10-17 13:23:32 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+class Driver {
+	constructor(name, dateStarted) {
+		this.name = name;
+		this.startDate = new Date(dateStarted);
+	}
+
+	yearsExperienceFromBeginningOf(year) {
+		const diff = new Date(`Jan 1, ${year}`) - this.startDate;
+		const millisecsPerYear = 3.154e+10;
+		return Math.round(diff/millisecsPerYear);
+	} 
+}
+
+class Route {
+
+	constructor(beginningLocation, endingLocation) {
+		 this.beginning = beginningLocation;
+		 this.ending = endingLocation;
+	}
+
+	blocksTravelled() {
+		let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+		
+		const northSouthDist = Math.abs(parseInt(this.ending.vertical, 10) - parseInt(this.beginning.vertical, 10));
+
+		let beginningIndex = 0;
+		let endingIndex = 0;
+		eastWest.forEach(function(avenue, i, arr) {
+			if (arr[i] === this.beginning.horizontal) {
+				beginningIndex = i;
+			}
+			if (arr[i] === this.ending.horizontal) {
+				endingIndex = i;
+			}
+		}, this);
+		const eastWestDist = Math.abs(endingIndex -  beginningIndex);
+		return northSouthDist + eastWestDist;
+	}
+
+	estimatedTime(peak = false) {
+
+		const peakRate = 2;
+		const offPeakRate = 3;
+
+		if (peak) {
+			return this.blocksTravelled() / peakRate;
+		} else {
+			return this.blocksTravelled() / offPeakRate;
+		}
+	}  
+}
+
+
+
+
+
 
