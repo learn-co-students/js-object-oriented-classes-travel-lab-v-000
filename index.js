bf207ccea9cc3f6974e2af7ef5019bafa5ae31b4
Travis Cole <iamtraviscole@gmail.com>
2017-10-17 04:10:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    const endDate = new Date (year, 0, 1)
+    return Math.floor((endDate - this.startDate) / (24*60*60*1000*365))
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal)
+    const vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+    return Math.abs(horizontal + vertical)
+  }
+
+  estimatedTime(peak) {
+    if (peak){
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
