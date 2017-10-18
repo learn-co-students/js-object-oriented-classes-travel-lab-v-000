48695998de87cb815a036bfbfded7e63f9f3c863
Dominique McCloud <niquelejon@gmail.com>
2017-10-18 16:21:17 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 
+  class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate =  new Date(new Date(startDate).getFullYear(), 0, 1);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    console.log(this.startDate)
+    return year - this.startDate.getFullYear()-1
+  }
+}
+
+  let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+class Route {
+  constructor(beginningLocation, endingLocation){
+      this.beginningLocation = beginningLocation
+      this.endingLocation = endingLocation
+    }
+    avenueToInteger(avenue){
+      return eastWest.indexOf(avenue)
+    }
+    blocksTravelled(){
+      let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+      let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+      return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+    }
+    estimatedTime(peak){
+      if(peak){
+        return this.blocksTravelled()/2
+      } else {
+        return this.blocksTravelled()/3
+      }
+    }
+  }
