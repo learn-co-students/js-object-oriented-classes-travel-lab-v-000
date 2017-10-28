ac758f97aec1b4adb4af2ee6f01e8ff5532e75f1
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-28 18:14:16 -0400

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   }
-  yearsExperienceFromBeginningOf() {
-    
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
   }
+}
+
+// East to west
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+/
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    // both of these have attributes of horizontal and vertical
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  // returns an integer with the number of blocks travelled
+  //blocksTravelled(){}
+  // streets we can subtract, ie: 15th - 10th
 }
