539dc3a627f1b37ff55352db67ba16243549604c
laramontana <larabrightside@gmail.com>
2017-11-08 22:22:06 +0300

    Done.

--- index.js
+++ index.js
@@ -21,21 +21,22 @@ class Route {
 // endingLocation.vertical: '42'
 
   blocksTravelled() {
-    debugger
+
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     function horizontalToIndex(location) { return eastWest.indexOf(location) }
-   
-    const beginningHorizontal = horizontalToIndex(self.beginningLocation.horizontal)
-    const endingHorizontal = horizontalToIndex(self.endingLocation.horizontal)
+
+    const beginningHorizontal = horizontalToIndex(this.beginningLocation.horizontal)
+    const endingHorizontal = horizontalToIndex(this.endingLocation.horizontal)
     const horizontalBlocks = endingHorizontal - beginningHorizontal
 
-    const verticalBlocks = self.endingLocation.vertical - self.beginningLocation.vertical
+    const verticalBlocks = this.endingLocation.vertical - this.beginningLocation.vertical
 
     return horizontalBlocks + verticalBlocks
   }
 
-  estimatedTime() {
-
+  estimatedTime(peak) {
+    if (peak) { return this.blocksTravelled()/2 }
+    else { return this.blocksTravelled()/3 }
   }
 
 
