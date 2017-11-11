a75612ed52714ed226e98f9c15bfd804dcb5add7
leog7one <lguzman@me.com>
2017-11-10 20:00:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year,0,1)
+    let totalYears = (endDate - this.startDate) / (365*24*60*60*1000)
+    let roundedTotalYears = Math.round(totalYears);
+    return roundedTotalYears;
+  }
+}
+
+class Route {
+  //Both of these arguments are JavaScript objects with attributes of horizontal and vertical.
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue);
+  }
+  
+  blocksTravelled() {
+    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal); 
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return horizontalDistance + verticalDistance;
+  }
+  estimatedTime(peak) {
+   if (peak) {
+     return this.blocksTravelled() / 2;
+   } else {
+     return this.blocksTravelled() / 3;
+   }
+ }
+}
