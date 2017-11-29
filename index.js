d5355e211ac2232c00039aba5ffc1bdf40e5e4df
tracy p holmes <tracypholmes@gmail.com>
2017-11-29 08:57:55 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+let eastWest = [
+  "1st Avenue",
+  "2nd Avenue",
+  "3rd Avenue",
+  "Lexington Avenue",
+  "Park",
+  "Madison Avenue",
+  "5th Avenue"
+];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year) {
+    const endDate = new Date(year, 1, 1)
+    const totalYears = (endDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
+    return parseInt(totalYears)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  // I got confused on these next two methods, 
+  // so need to practice a bit more with my maths, etc.
+  avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled(){
+    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+  }
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
