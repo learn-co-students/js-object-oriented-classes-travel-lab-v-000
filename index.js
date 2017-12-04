513e0872732e9e57cad5165f6d907b7954deaac4
Elias Ventura <eliasventura23@gmail.com>
2017-12-04 19:52:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.startDate = new Date(date)
+}
 
+yearsExperienceFromBeginningOf(year) {
+  let startYear = this.startDate.getFullYear()
+
+  return [year - startYear] -1;
+
+}
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+
+    }
+
+    blocksTravelled() {
+        let avenuesTravelled = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+
+        let streetsTravelled = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+
+        let totalBlocks = avenuesTravelled + streetsTravelled;
+
+        return totalBlocks;
+
+    }
+
+    estimatedTime (peak) {
+      if(peak){
+      return this.blocksTravelled()/2;
+    }
+    else {
+      return this.blocksTravelled()/3;
+    }
+}
+}
