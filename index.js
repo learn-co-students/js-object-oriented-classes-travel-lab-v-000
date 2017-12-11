858bfe0a541bcb472dc4067b6fb68d050953ce54
Cynthia Rittenbach <crittenbach@gmail.com>
2017-12-11 21:03:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+   constructor(name, startDate) {
+     this.name = name;
+     this.startDate = new Date(startDate);
+   };
 
+   yearsExperienceFromBeginningOf(endDate) {
+     const years = endDate - this.startDate.getFullYear() -1;
+     return years;
+   };
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    const vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+    const horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    const travelled = vertical + horizontal;
+    return travelled;
+  }
+
+ estimatedTime(peakHours) {
+
+  if (peakHours === true) {
+    const estimate = this.blocksTravelled() / 2;
+    return estimate;
+  } else {
+    const estimate = this.blocksTravelled() / 3;
+    return estimate;
+  }
+
+}
+
+};
