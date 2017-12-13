4c1e7016bfe94dd0ec7642fe3ed234b567f813c7
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-12-12 22:09:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    const startingEastWest = eastWest.indexOf(this.beginningLocation.horizontal);
+    const endingEastWest = eastWest.indexOf(this.endingLocation.horizontal);
+    const horizontalBlocksTravelled = Math.abs( startingEastWest - endingEastWest );
+    const verticalBlocksTravelled = Math.abs( this.beginningLocation.vertical - this.endingLocation.vertical);
+    return horizontalBlocksTravelled + verticalBlocksTravelled;
+  }
+
+  estimatedTime(peak) {
+    if (peak === true) {
+      return Math.floor(this.blocksTravelled() / 2);
+    } else {
+      return Math.floor(this.blocksTravelled() / 3);
+    }
+  }
+
+}
