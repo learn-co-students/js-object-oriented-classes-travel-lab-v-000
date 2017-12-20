87371e1e1243c1ea095bbe0c7c1960edb06a42de
Sophia Varella <sophiavarella@gmail.com>
2017-12-19 18:54:59 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    
+    let horizontalBlocks = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    let verticalBlocks = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let totalBlocks = Math.abs(horizontalBlocks) + Math.abs(verticalBlocks);
+
+    return totalBlocks;
+  }
+
+  estimatedTime(peak) {
+    return peak ? (this.blocksTravelled() / 2) : (this.blocksTravelled() / 3)
+  }
+}
