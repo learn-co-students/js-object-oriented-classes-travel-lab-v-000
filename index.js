4e64379234a0264c4f03bf9b19e481a16027abfe
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-05 17:41:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.date = date;
+    this.startDate = new Date(this.date);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    const horizontal = Math.abs((eastWest.indexOf(this.beginningLocation.horizontal) + 1) - (eastWest.indexOf(this.endingLocation.horizontal) + 1))
+    const vertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+
+    return horizontal + vertical
+  }
+
+  estimatedTime(peakHours) {
+    const blocks = this.blocksTravelled()
+
+    if (peakHours) {
+      return blocks / 2
+    } else {
+      return blocks / 3
+    }
+  }
+}
