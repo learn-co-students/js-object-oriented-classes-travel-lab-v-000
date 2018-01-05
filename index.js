6d50a9ab97eeb75bddf00628489235df6abf6e96
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2018-01-05 14:11:14 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let beginningYear = this.startDate.getFullYear();
+    return year - beginningYear;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let upDown = parseInt(this.endingLocation.vertical - this.beginningLocation.vertical)
+    let leftRight = parseInt(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal))
+    return upDown + leftRight;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+        return this.blocksTravelled()/2;
+    } else {
+        return this.blocksTravelled()/3;
+    }
+  }
+
+}
