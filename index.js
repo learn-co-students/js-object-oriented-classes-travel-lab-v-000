3be45d5696d06ab72e1c916231943c2418700954
dbfarms <matt.hunger@gmail.com>
2018-01-12 13:59:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
+    constructor(name, startDate) {
+        this.name = name
+        this.startDate = new Date(startDate)
+    }
+    
+    yearsExperienceFromBeginningOf(endDate) {
+        endDate = new Date(endDate)
+        return endDate - this.startDate.getFullYear()
+    }
+}
 
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+        
+    }
+    
+    avenueToInteger(avenue) {
+        let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+        return eastWest.indexOf(avenue)
+    }
+    
+    blocksTravelled(){
+        let vertical = this.beginningLocation.vertical - this.endingLocation.vertical
+        let horizontal = this.avenueToInteger(this.beginningLocation.horizontal) - this.avenueToInteger(this.endingLocation.horizontal)
+        
+        return (Math.abs(horizontal) + Math.abs(vertical)) 
+    }
+    
+    estimatedTime(peakHours){
+        if (peakHours) {
+            return (this.blocksTravelled() / 2)
+        } else {
+            return (this.blocksTravelled() / 3)
+        }
+    }
+    
+}
