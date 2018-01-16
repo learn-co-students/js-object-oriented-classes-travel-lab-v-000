d44ce3000c601a207fa25f132ef4df45898327d1
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 04:31:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+class Driver {
+  constructor (name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let horizontalBlocks = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal)
+    let verticalBlocks = parseInt(this.endingLocation.vertical) - parseInt(this.beginningLocation.vertical)
+    return Math.abs(horizontalBlocks) + Math.abs(verticalBlocks)
+  }
+
+  estimatedTime(peakTime) {
+    return peakTime ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3;
+  }
+}
