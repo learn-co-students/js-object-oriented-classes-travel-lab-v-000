c88373d5a718cbfea6a3881a39a13f9af0f771b2
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-01-16 15:49:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
+class Driver {
+    constructor(name,startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
+    yearsExperienceFromBeginningOf(year) {
+        // return int
+        return year - this.startDate.getFullYear();
+    }
+}
+
+class Route {
+    constructor(beginningLocation,endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    }
+    blocksTravelled() {
+        // return int
+        return Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal) ) + Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    }
+    estimatedTime(peak = false) {
+        if (peak) {
+            return this.blocksTravelled() / 2
+        }
+        else {
+            return this.blocksTravelled() / 3
+        }
+    }
+}
