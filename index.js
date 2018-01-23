a5adca62a65ec4be991e11cd8ea1ca536431a627
kromoser <kevinromoser@gmail.com>
2018-01-23 00:51:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    const end = new Date(year)
+    const start = this.startDate.getFullYear()
+    return end.getTime() - start;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const startingAvenue = this.beginningLocation.horizontal
+    const endingAvenue = this.endingLocation.horizontal
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const startingAveValue = eastWest.findIndex(function(element) {
+      return element === startingAvenue
+    });
+    const endingAveValue = eastWest.findIndex(function(element) {
+      return element === endingAvenue
+    });
+    const eastWestDistance = Math.abs(endingAveValue - startingAveValue)
+    const northSouthDistance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
+    return northSouthDistance + eastWestDistance
+  }
+
+  estimatedTime(arg) {
+    const distance = this.blocksTravelled()
+    if (arg) {
+      return distance / 2
+    } else {
+      return distance / 3
+    }
+  }
+
+}
