83b8219411b9fc5320bae424c2bc1f2fad69dc5f
kyle-poretto <kyleporetto@gmail.com>
2018-01-27 14:49:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(`${startDate}`);
+  }
+
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000) + 1
+    return parseInt(totalYears)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled (){
+    let eastWestTravel = Math.abs(this.avenueToInteger(this.beginningLocation.horizontal) - this.avenueToInteger(this.endingLocation.horizontal))
+    let northSouthTravel = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return eastWestTravel + northSouthTravel
+      }
+      
+  estimatedTime(peak){
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+  }}
+  }
+
+  
 
