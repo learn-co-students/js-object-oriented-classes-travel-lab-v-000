f38e296f14a41fd15f5d056b997ea81b32151afd
Igor Eskin <github email address>
2018-02-01 22:52:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
 
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+
+}
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let horizontalDistance = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    let verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    let distance = horizontalDistance + verticalDistance;
+    return distance;
+  }
+
+  estimatedTime(peak) {
+    if (peak === true) {
+      return Math.floor(this.blocksTravelled() / 2);
+    } else {
+      return Math.floor(this.blocksTravelled() / 3);
+    }
+  }
+  
+}
