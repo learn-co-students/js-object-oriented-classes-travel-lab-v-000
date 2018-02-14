cb5f2652f658a7b0a0dd31988810771fa67bc52e
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-14 13:31:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
 
+    const date = Date.parse(startDate);
+    this.startDate = new Date(date);
+  };
+  yearsExperienceFromBeginningOf(year) {
+    // console.log(this.startDate.getFullYear() - year);
+    return year - this.startDate.getFullYear();
+  };
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let horizontalDistance = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    let verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    return horizontalDistance + verticalDistance
+    // console.log(eastWest)
+    // console.log(this.beginningLocation.horizontal);
+    // console.log(this.endingLocation.horizontal);
+    // console.log(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+  };
+  estimatedTime(peakTime) {
+    if (peakTime === true) {
+      return this.blocksTravelled()/2
+    }
+    else {
+      return this.blocksTravelled()/3;
+    }
+  };
+};
