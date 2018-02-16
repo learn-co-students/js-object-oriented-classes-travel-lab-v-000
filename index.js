e7c772bc4809c97f29fca42757743c6cb85b9c2f
Alexander Friedland <friedlandaj@gmail.com>
2018-02-16 05:55:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+class Driver {
+  constructor (name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let date = new Date(year, 0, 1).getFullYear();
+    return date - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled () {
+    
+  }
+}
