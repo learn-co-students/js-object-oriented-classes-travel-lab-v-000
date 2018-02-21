e9497a2cd85a894c5751f0d422b0bd40f1e75fad
Hannah Carnes <hecarnes@gmail.com>
2018-02-20 20:46:33 -0500

    driver and route class working

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return (endDate - this.startDate.getFullYear())
+  }
+}
+
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let horizontalBlocksTravelled = this.horizontalBlocks(this.beginningLocation.horizontal, this.endingLocation.horizontal)
+    let verticalBlocksTravelled = this.verticalBlocks(this.beginningLocation.vertical, this.endingLocation.vertical)
+
+    return horizontalBlocksTravelled + verticalBlocksTravelled
+  }
+
+  horizontalBlocks(beginningHorizontalLocation, endingHorizontalLocation) {
+    return eastWest.indexOf(endingHorizontalLocation) - eastWest.indexOf(beginningHorizontalLocation)
+  }
+
+  verticalBlocks(beginningVerticalLocation, endingVerticalLocation) {
+    return parseInt(endingVerticalLocation, 10) - parseInt(beginningVerticalLocation, 10)
+  }
+
+}
