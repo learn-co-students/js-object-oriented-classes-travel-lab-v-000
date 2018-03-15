42bd799abb513f301358ba211380a6c59eb28a90
github username <github email address>
2018-03-15 16:37:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Driver {
   constructor(name, startDate) {
     this.name = name
@@ -16,23 +18,16 @@ class Route {
   }
 
   blocksTravelled(){
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-
     let horizontalDistance = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal);
-
     let verticalDistance = this.beginningLocation.vertical - this.endingLocation.vertical;
-
-    let result = Math.abs(verticalDistance) + Math.abs(horizontalDistance);
-
-    return result;
+    return Math.abs(verticalDistance) + Math.abs(horizontalDistance);;
   }
 
-  estimatedTime(boolean) {
-    if (boolean === true) {
+  estimatedTime(peak) {
+    if (peak) {
       return this.blocksTravelled() / 2;
     } else {
         return this.blocksTravelled() / 3;
     }
   }
-
 }
