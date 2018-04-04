b987cec57aa0cec74eab65da1ace544109e3c062
github username <github email address>
2018-04-04 17:12:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+
+class Driver {
+  constructor(name, dateString){
+    this.name = name;
+    this.startDate = new Date(dateString);
+  }
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  //get the index of the avenue separately as an integer to do le subtraction
+  avenueToInteger(avenue){
+   return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled(){
+   const horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+   const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+   return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+  }
+
+  estimatedTime(){
+    
+  }
+}
