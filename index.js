f784b2c2f99107b76309b85096ef828966f30948
github username <github email address>
2018-04-05 12:11:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
-
 class Driver {
   constructor(name, dateString){
-    this.name = name;
-    this.startDate = new Date(dateString);
+    this.name = name
+    this.startDate = new Date(dateString)
   }
+
   yearsExperienceFromBeginningOf(year){
-    return year - this.startDate.getFullYear();
+    const endDate = new Date(year, 1, 1)
+    const totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
   }
+
 }
 
 class Route {
-  constructor(beginningLocation,endingLocation){
+  constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
 
-  //get the index of the avenue separately as an integer to do le subtraction
   avenueToInteger(avenue){
-   return eastWest.indexOf(avenue)
+    return eastWest.indexOf(avenue)
   }
 
   blocksTravelled(){
-   const horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
-   const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
-   return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+    const horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
   }
 
-  estimatedTime(){
-    
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
   }
+
 }
