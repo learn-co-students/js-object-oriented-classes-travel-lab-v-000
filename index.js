df1caa02895253a142ad378cfc8014e383b8069d
Sam Marshall <samuel.r.marshall@gmail.com>
2018-04-19 21:26:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+class Driver {
 
+  constructor(name,day){
+    this.name = name;
+    this.day = day;
+    this.startDate = new Date(day);
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear();
+  }
+
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+class Route {
+
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    return beginningLocation.vertical
+  }
+
+  estimatedTime(){
+
+  }
+
+}
