7f3ab65769b3f81b9d9156f461564372c2aad83e
catalanowebdevelopment <catalano.work@gmail.com>
2018-05-01 13:56:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Driver {
+    constructor(name, startDate) {
+        this.name = name
+        this.startDate = new Date(startDate)
+    }
+
+    yearsExperienceFromBeginningOf(year) {
+        let endDate = new Date(year, 0, 1)
+        let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+        return parseInt(totalYears)
+    }
+}
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+    }
+    
+    avenueToInteger(avenue) {
+        return eastWest.indexOf(avenue)
+    }
+    
+    blocksTravelled() {
+        debugger
+    }
+}
+
 
