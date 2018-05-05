406e35db1d4703262228478432a7556a8da87e68
jacobsilver2 <jacobsilver2@mac.com>
2018-05-05 15:07:42 -0400

    final step

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Driver {
   constructor(name, start) {
     this.name = name;
@@ -9,11 +11,17 @@ class Driver {
     let years = (end - this.startDate)/(365*24*60*60*1000)
     return parseInt(years)
   }
-
-
-
-
+  
 }
 
+class Route {
+  constructor(beg, end) {
+    this.beginningLocation = beg;
+    this.endingLocation = end;
+  }
 
+  blocksTravelled(){
+        
+  }  
+}
 
