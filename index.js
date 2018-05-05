5c251a1984d0fcf8c5369c17166dec0f3a2a94ae
jacobsilver2 <jacobsilver2@mac.com>
2018-05-05 15:17:18 -0400

    done

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ class Route {
     return vert + horiz;
   }  
 
-  estimatedTime(){
-    
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
   }
 }
 
