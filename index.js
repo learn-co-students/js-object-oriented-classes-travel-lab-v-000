0775f06e28ec8db2969f380f7dfce57546529fc0
jacobsilver2 <jacobsilver2@mac.com>
2018-05-05 14:55:46 -0400

    dfdf

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ class Driver {
   }
 
   yearsExperienceFromBeginningOf(year) {
-    
+    let end = new Date(year, 1, 1)
+    let years = (end - this.startDate)/(365*24*60*60*1000)
+    return parseInt(years)
   }
 
 
 
 
 }
+
+
+
