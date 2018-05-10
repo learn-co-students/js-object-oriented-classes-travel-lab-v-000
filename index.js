116d1439a13a67d321eb859746494ef5fd6031f0
Allyson Wesman <allysonwesman@gmail.com>
2018-05-09 17:24:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  startDate() {
+    return startDate;
+  }
+
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+    const nsBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+
+    const ewBeginIndex = eastWest.indexOf(this.beginningLocation.horizontal);
+    const ewEndIndex = eastWest.indexOf(this.endingLocation.horizontal);
+    const ewBlocks = Math.abs(ewEndIndex - ewBeginIndex);
+
+    return nsBlocks + ewBlocks;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+
+}
