4a9f24671f0a59ddcbe39542aa6236afc338462d
Salma Elshahawy <salmaeng71@gmail.com>
2018-05-23 13:28:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+class Driver {
+  constructor (name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+yearsExperienceFromBeginningOf(endDate) {
+  
+  //this converts endDate to a string and then creates the date object and pulls only the year out
+    //it then subtracts the startDate year attribute from it
+    return new Date(`${endDate}`).getFullYear() - this.startDate.getFullYear()
+  }
+}
