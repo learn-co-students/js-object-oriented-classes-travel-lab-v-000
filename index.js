7cdd9ad071cf757ed2d3925fbfe2b587f07a00e5
ecssiah <ecssiah@gmail.com>
2018-06-01 23:01:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 
+      'Madison Avenue', '5th Avenue'
+    ];
+
+    const vertDistance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical); 
+    const horzDistance = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal) );
+
+    return vertDistance + horzDistance; 
+  }
+
+  estimatedTime(peakHours = false) {
+    if (peakHours) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
