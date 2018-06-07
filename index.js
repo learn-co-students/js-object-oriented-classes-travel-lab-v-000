c264d5ed37684c1612badb04970843fa2559726d
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-06-06 17:23:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+    constructor(name, startDate){
+        this.name = name
+        this.startDate = new Date(startDate)
+    }
+    yearsExperienceFromBeginningOf(year){
+        return year - this.startDate.getFullYear()
+    }
+}
 
+class Route {
+    constructor(beginningLocation, endingLocation){
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+    }
+
+    blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+        function avenueNumber(street){
+            return eastWest.indexOf(street);
+        }
+        let horizontalDistance = avenueNumber(this.endingLocation.horizontal) - avenueNumber(this.beginningLocation.horizontal)
+        let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+        return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
+    }
+
+    estimatedTime(peak){
+        if(peak){
+            return this.blocksTravelled()/2
+        } else {
+            return this.blocksTravelled()/3
+        }
+      }
+}
