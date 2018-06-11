7ffd3eb004048a70a90f4eeda5d2dfc7e004cd1c
nmlstyl <joey.michalina.mariano@gmail.com>
2018-06-10 22:27:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    let start = this.startDate.getFullYear();
+    return endDate - start;
+  }
+}
+
+class Route{
+
+}
