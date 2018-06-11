c0f73c14160baff576d1f7afd6f3dcbf20795d6a
nmlstyl <joey.michalina.mariano@gmail.com>
2018-06-11 01:10:09 -0400

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,36 @@ class Route{
   }
 
   blocksTravelled() {
-    return this.end.vertical - this.start.vertical
+    let startHoriz;
+    let endHoriz;
+
+    if (this.end.horizontal === "Park"){
+      endHoriz = 5;
+    } else {
+      endHoriz = parseInt(this.end.horizontal[0]);
+    }
+
+    if (this.start.horizontal === "Park"){
+      startHoriz = 5;
+    } else {
+      startHoriz = parseInt(this.start.horizontal[0]);
+    }
+
+    let verticalBlocks = this.end.vertical - this.start.vertical;
+    let horizontalBlocks = endHoriz - startHoriz;
+
+    return verticalBlocks + horizontalBlocks;
+  }
+
+  estimatedTime(peak){
+    let result;
+    let blocks = this.blocksTravelled();
+
+    if (peak === true){
+      result = blocks / 2;
+    } else {
+      result = blocks / 3;
+    }
+    return result;
   }
 }
