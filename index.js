396c99c9054ffebb3b28057db228ac7b62de34a4
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-30 10:04:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+class Driver {
+  constructor (name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf (year) {
+    return year - this.startDate.getFullYear();
+  }
+}
 
+class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled() {
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    let hor1 = eastWest.findIndex(el => el == this.beginningLocation.horizontal);
+    let hor2 = eastWest.findIndex(el => el == this.endingLocation.horizontal);
+    return (this.endingLocation.vertical - this.beginningLocation.vertical) + (hor2 -hor1);
+  }
+  estimatedTime(peak) {
+    if (peak === true) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    }
+  };
+}
