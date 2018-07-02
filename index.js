b4a84762e6dca9e33fed0b6a0bd41c22532cc114
e-be-walk <e.be.walk@gmail.com>
2018-07-01 22:48:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear()
+  }
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled() {
+    let horiDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let vertDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horiDistance) + Math.abs(vertDistance))
+  }
+  estimatedTime(peak) {
+    if (peak === true) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    }
+  };
+}
