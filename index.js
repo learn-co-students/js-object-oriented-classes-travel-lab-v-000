a00d033a3a1b439ba8e054c03c9f51773add6086
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-07-13 21:15:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year){
+    return (year- this.startDate.getFullYear())
+  }
+}
+
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  streetToInteger(street){
+    let eastWestArray = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    
+    return eastWestArray.indexOf(street)
+  }
+  
+  blocksTravelled(){
+    let northSouth = this.beginningLocation.vertical - this.endingLocation.vertical
+    
+    let eastWest = this.streetToInteger(this.beginningLocation.horizontal) - this.streetToInteger(this.endingLocation.horizontal)
+
+    return (Math.abs(northSouth) + Math.abs(eastWest))
+  }
+  
+  estimatedTime(peak = false){
+    if(peak === true ){
+      return this.blocksTravelled() / 2
+    }else{
+      return this.blocksTravelled() / 3 
+    }
+  }
+}
