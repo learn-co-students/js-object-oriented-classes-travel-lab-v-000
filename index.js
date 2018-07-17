6e00cc604054c023ee1ad96552d86ed2cf7a854d
Kim Gonzales <Kimgo589@gmail.com>
2018-07-17 18:11:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name,stringDate){
+    this.name = name;
+    this.startDate = new Date(stringDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getUTCFullYear();
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  horizontalToInt(street){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    return eastWest.indexOf(street);
+  }
+
+  blocksTravelled(){
+    let horizontalDist = this.horizontalToInt(this.endingLocation.horizontal) - this.horizontalToInt(this.beginningLocation.horizontal);
+    let verticalDist = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return (Math.abs(horizontalDist) +  Math.abs(verticalDist))
+  }
+
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
