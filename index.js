6690b302f6e138a615e89c5e6b58612e995dad87
Rygel-XVI <micha232@umn.edu>
2018-07-20 21:09:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+class Driver {
+
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(endDate) {
+    const d = new Date()
+    d.setYear(endDate)
+    return Math.abs(this.startDate.getFullYear() - d.getFullYear());
+  }
+
+}
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    const xDist = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    const yDist = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return xDist + yDist;
+  }
+
+  estimatedTime(peak) {
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+
+}
