7f836bf02d8b2966fa0fe6af2228718fc0a16386
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-07-23 11:26:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+class Driver {
+  constructor(name, day){
+    this.name = name
+    this.day = day
+    this.startDate = new Date(day)
+   }
+
+   yearsExperienceFromBeginningOf(endDate){
+     return endDate - this.startDate.getFullYear()
+   }
+  }
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    const beginningHorizontal = eastWest.indexOf(this.beginningLocation.horizontal);
+    const beginningVertical = this.beginningLocation.vertical;
+    const endingHorizontal = eastWest.indexOf(this.endingLocation.horizontal);
+    const endingVertical = this.endingLocation.vertical;
+
+    const distHorizontal = Math.abs(beginningHorizontal - endingHorizontal);
+    const distVertical = Math.abs(beginningVertical - endingVertical);
+
+    return distVertical + distHorizontal;
+  }
+
+  estimatedTime(peak = false) {
+    if (peak) return this.blocksTravelled() / 2;
+    return this.blocksTravelled() / 3;
+  }
+}
