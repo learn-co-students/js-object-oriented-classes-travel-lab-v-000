45a93eae1cc32aad2433d08c844bbcaac47d2a6a
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-28 16:44:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+class Driver {
+  constructor(name, startDate){
+  this.name = name
+  this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    return (new Date(year) - this.startDate)
+  }
+}
+
+
+
+
+class Routes {
+  constructor(beginningLocation, endingLocation){
+  this.beginningLocation = beginningLocation
+  this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+
+  }
+
+  estimatedTime(){
+
+  }
+}
