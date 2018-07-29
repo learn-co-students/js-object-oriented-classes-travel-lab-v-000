89e3e3a49cd4803be119c9a28e5b64a9e9146b25
fonzmp <fonzmp@gmail.com[color]>
2018-07-28 22:45:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    const startDate = this.startDate;
+    return ((year - startDate.getFullYear()) - 1)
+  }
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+
+  constructor(start, end) {
+    this.start = start
+    this.end = end
+  }
+
+  blocksTravelled() {
+
+
+    const verticalDistance = (this.end.vertical - this.start.vertical)
+    const horizontalDistance = (eastWest.indexOf(this.end.horizontal) - eastWest.indexOf(this.start.horizontal))
+    return verticalDistance + horizontalDistance;
+  }
+
+  estimatedTime(boolean) {
+    if (boolean !== true) {
+      return this.blocksTravelled() / 3
+    }
+    else {
+      return this.blocksTravelled() / 2
+    }
+  }
+}
