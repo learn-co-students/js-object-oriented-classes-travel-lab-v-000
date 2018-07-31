8c7cadf95c861c13f2cb9f78db4f53da9e248284
TrevorSeitz <trevinla@gmail.com>
2018-07-31 19:07:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return (endDate - this.startDate.getFullYear() -1)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+    }
+
+
+  blocksTravelled(beginningLocation, endingLocation) {
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const horDist = Math.abs( this.beginningLocation.vertical - this.endingLocation.vertical )
+    const vertDist = Math.abs( eastWest.indexOf(this.beginningLocation.horizontal ) - eastWest.indexOf(this.endingLocation.horizontal) )
+    return horDist + vertDist
+  }
+
+  estimatedTime(rushHour) {
+    if (rushHour) {
+      return this.blocksTravelled(this.beginningLocation, this.endingLocation)/2
+    } else {
+      return this.blocksTravelled(this.beginningLocation, this.endingLocation)/3
+    }
+  }
+
+
+
+}
