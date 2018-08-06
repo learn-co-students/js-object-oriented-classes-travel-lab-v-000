2257266ba62a4909a90656dfb85ef350234ac84b
Diego Yanez <diegoyanez@MacBook-Pro-5.local>
2018-08-06 17:57:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear() -1
+  }
+}
 
+
+
+  let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue']
+
+class Route {
+  constructor(start, end){
+    this.start = start;
+    this.end = end;
+  }
+
+  blocksTravelled(){
+    const verticalDistance = (this.end.vertical - this.start.vertical)
+    const horizontalDistance = (eastWest.indexOf(this.end.horizontal) - eastWest.indexOf(this.start.horizontal))
+
+    return verticalDistance + horizontalDistance
+  }
+
+  estimatedTime(peak = false){
+    if (peak === true){
+      return this.blocksTravelled()/2
+    } else{
+      return this.blocksTravelled()/3
+    }
+  }
+}
