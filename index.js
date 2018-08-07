0ccedbcbeaf90fa6e1d295d63454b4ffdd850669
Dee Race <deerace12@gmail.com>
2018-08-07 14:55:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - `${this.startDate.getFullYear()}` - 1
+  }
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  avenueToInt(ave) {
+    return eastWest.indexOf(ave)
+  }
+
+  blocksTravelled() {
+
+    let horizontalDist = this.avenueToInt(this.endingLocation.horizontal) - this.avenueToInt(this.beginningLocation.horizontal)
+    let verticalDist = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDist) + Math.abs(verticalDist))
+  }
+
+  estimatedTime(peak) {
+    if(peak) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+
+}
