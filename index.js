ca6a89dfcc93e737b704e00486e1075e992dd4d6
ktamoguis <ktamoguis@gmail.com>
2018-08-13 11:59:20 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+class Driver {
+  constructor (name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endYear){
+    return endYear - this.startDate.getFullYear()  -1 ;
+  }
+
+}
+
+class Route {
+  constructor (beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  //let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+  blocksTravelled(){
+    const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    console.log(this.beginningLocation.horizontal)
+    debugger;
+  }
+}
