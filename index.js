5e24245d5181f0afe508993a076228732b553581
ktamoguis <ktamoguis@gmail.com>
2018-08-13 12:31:02 +0800

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,25 @@ class Route {
   //let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
   blocksTravelled(){
     const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-    console.log(this.beginningLocation.horizontal)
-    debugger;
+    const beginHorizontal = this.beginningLocation.horizontal;
+    const endHorizontal = this.endingLocation.horizontal;
+
+    let distVertical = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+
+    let distHorizontal = Math.abs(eastWest.indexOf(beginHorizontal) - eastWest.indexOf(endHorizontal));
+
+
+    return distVertical + distHorizontal;
+  }
+
+  estimatedTime(peakTime = false){
+      //debugger;
+      if (peakTime){
+        //debugger;
+        return this.blocksTravelled() / 2;
+      } else {
+        //debugger;
+        return this.blocksTravelled() / 3;
+      }
   }
 }
