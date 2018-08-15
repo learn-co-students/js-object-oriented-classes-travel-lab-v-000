b18491af75a00292056d6f853478300b0b161b78
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-15 16:49:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+   constructor(name, startDate) {
+     this.name = name
+     this.startDate = new Date(startDate);
+   }
 
+   yearsExperienceFromBeginningOf(endDate) {
+    return endDate - `${this.startDate.getFullYear()}` - 1;
+  }
+}
+
+
+let eastWest= ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue', '6th Avenue', '7th Avenue','8th Avenue','9th Avenue','10th Avenue']
+
+class Route {
+  constructor(beginningLocation,endingLocation){
+      this.beginningLocation = beginningLocation
+      this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled() {
+    let horizontalChange = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let verticalChange = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalChange)) + (Math.abs(verticalChange))
+  }
+
+  estimatedTime(peak){
+    if(peak) {
+      return this.blocksTravelled()/2
+    }
+    else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
