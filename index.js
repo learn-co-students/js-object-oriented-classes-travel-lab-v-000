31a6be21f503e474b7567b27714fd2587fbcdcca
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-08-17 11:35:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return new Date(endDate, 1, 1).getYear() - this.startDate.getYear() -1
+  }
+
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+  constructor(startLocation, endLocation){
+    this.startLocation = startLocation;
+    this.endLocation = endLocation;
+  }
+
+  blocksTravelled(){
+  let horizontal = eastWest.indexOf(this.endLocation["horizontal"]) - eastWest.indexOf(this.startLocation["horizontal"])
+  let vertical = this.endLocation["vertical"] - this.startLocation["vertical"]
+
+  return horizontal + vertical
+  }
+
+  estimatedTime(peak){
+  if (peak){
+    return  Math.round(this.blocksTravelled() / 2)
+  }
+    return Math.round(this.blocksTravelled() / 3)
+
+  }
+}
+// During off peak hours, a driver travels three blocks in a minute,
+// while during peak hours a driver travels an estimated two blocks in a minute.
