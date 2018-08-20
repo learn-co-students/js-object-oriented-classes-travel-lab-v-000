3be181b6e8a23cbf699fe48bf34edac5763754d9
allisonkadel <owner@allisonkadel.com>
2018-08-20 05:28:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+
+class Driver {
+    constructor(name,joined) {
+        this.name = name;
+        this.startDate = new Date(joined);
+    }
+
+    yearsExperienceFromBeginningOf(year) {
+        return year - this.startDate.getFullYear() - 1
+    }
+}
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+        
+    }
+
+
+    blocksTravelled() {
+        return (Math.abs(this.beginningLocation['vertical'] - this.endingLocation['vertical']) + Math.abs(eastWest.indexOf(this.beginningLocation['horizontal']) - eastWest.indexOf(this.endingLocation['horizontal'])))   
+    }
+
+    estimatedTime(onPeak) {
+        if (onPeak) {
+            return this.blocksTravelled()/2
+        } else {
+        return this.blocksTravelled()/3
+            }
+    }
+     
+}
