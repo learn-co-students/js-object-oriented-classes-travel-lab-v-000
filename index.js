68f0e26e343872ea5ae643cf48f764ec8d0e8044
github username <github email address>
2018-08-27 19:07:43 -0400

    working

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+class Driver{
+  constructor(name, startDate){
+  this.name = name
+  this.startDate = startDate
+  }
 
+
+  startDate() {
+   return new Date(this.startDate);
+   }
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+
+  }
+}
