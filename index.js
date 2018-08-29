b482c48a1c5d529e85e610fc202fba905d865afb
Katrina Brinson <kbrinso@gmail.com>
2018-08-29 17:18:34 +0000

    working on blocks traveled

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ class Route{
     this.endBlock = endBlock;
   }
   blocksTravelled(){
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    // console.log(eastWest);
+    // console.log(this.startBlock.horizontal);
+    var horizontalBlocks = Math.abs(eastWest.findIndex(x => this.startBlock.horizontal) - eastWest.findIndex(x => this.endBlock.horizontal));
+    var verticalBlocks =  Math.abs(this.startBlock.vertical - this.endBlock.vertical);
+    // console.log(eastWest.findIndex(x => this.startBlock.horizontal));
+    console.log(horizontalBlocks);
+    console.log(verticalBlocks);
+    return horizontalBlocks + verticalBlocks;
   }
 }
