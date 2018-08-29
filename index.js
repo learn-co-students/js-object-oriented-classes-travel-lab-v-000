fe48c99fb76180515cc33112c687ac052b4ca3aa
Katrina Brinson <kbrinso@gmail.com>
2018-08-29 16:55:38 +0000

    still working

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+    // console.log(this.startDate.getUTCFullYear());
+    // console.log(this.startDate.getFullYear(););
 
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getUTCFullYear();
+  }
+}
+
+class Route{
+  constructor(startBlock, endBlock){
+    this.startBlock = startBlock;
+    this.endBlock = endBlock;
+  }
+  blocksTravelled(){
+    
+  }
+}
