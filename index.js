ebed80d4017d9ed7d05b7c52e77005d83cd7abf2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-29 22:07:22 +0000

    Done.

--- index.js
+++ index.js
@@ -16,12 +16,18 @@ class Route {
   }
 
   blocksTravelled() {
-    let d1 = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
-let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-find beginning horizontal in east West
-find ending horizontal in east West
-and abs the difference
-  //  let route = new Route({horizontal: 'Park', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
-  //  expect(route.blocksTravelled()).to.equal(11)
+    const convertAvenue = function (avenue) {
+        const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+        return eastWest.findIndex( ave => ave===avenue);
+    }
+    const distanceVertical = Math.abs (this.beginningLocation.vertical - this.endingLocation.vertical);
+    const distanceHorizontal = Math.abs (convertAvenue(this.beginningLocation.horizontal) -
+          convertAvenue(this.endingLocation.horizontal));
+    return distanceVertical + distanceHorizontal;
+  }
+
+  estimatedTime( peakTime=false) {
+    const blocksPerMinute = peakTime ? 2 : 3;
+    return this.blocksTravelled() / blocksPerMinute;
   }
 }
