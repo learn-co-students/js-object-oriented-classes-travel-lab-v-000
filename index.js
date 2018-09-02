1fd0f41624973114591a25999f4ac846b5856602
kreopelle <kreopelle@gmail.com>
2018-09-02 17:12:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - (this.startDate.getFullYear() + 1 )
+  }
+
+}
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  avenueToInteger(avenue){
+
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    const verticalDistance = parseInt(this.beginningLocation.vertical) - parseInt(this.endingLocation.vertical)
+
+    const horizontalDistance = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal)
+
+    return Math.abs(verticalDistance) + Math.abs(horizontalDistance)
+  }
+
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+
+}
