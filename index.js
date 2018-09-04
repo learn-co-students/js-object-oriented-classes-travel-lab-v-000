cd1964d58b711e2d25a5fefea2f2440c8a0b3d48
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 17:57:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
+class Driver {
+  
+  constructor(name, day) {
+    this.name = name 
+    this.startDate = new Date(day)
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - (this.startDate.getFullYear()) - 1
+  }
+  
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation 
+    this.endingLocation = endingLocation
+  }
+  
+  
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  
+    
+    let hStart = Number.parseInt(this.beginningLocation['vertical'], 10) 
+    let hEnd = Number.parseInt(this.endingLocation['vertical'], 10)
+    
+    let h;
+    if (hStart < hEnd) {
+       h = hEnd - hStart
+    } else {
+      h =  hStart - hEnd
+    }
+    
+    let vStart = eastWest.indexOf(this.beginningLocation['horizontal'])
+    
+    let vEnd = eastWest.indexOf(this.endingLocation['horizontal'])
+    
+    let v;
+    if (vStart < vEnd) {
+     v = vEnd - vStart
+    } else {
+     v =  vStart - vEnd
+    }
+    
+    return v + h
+  }
+  
+  estimatedTime(peak) {
+    if (peak) {
+    return this.blocksTravelled()/2
+    } else {
+    return this.blocksTravelled()/3
+    }
+  }
+  
+}
