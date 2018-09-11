423c11e776d1e6bfd2989e6cf3dcc57c8450c04f
Kazumi Karbowski <kazkazinus@gmail.com>
2018-09-11 17:20:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+    constructor(name, startDate) {
+        this.name = name
+        this.startDate = new Date(startDate)
+    }
+
+    yearsExperienceFromBeginningOf(thisYear) {
+        let startYear = this.startDate.getFullYear()
+        return thisYear - startYear - 1
+    }
+}
+
+const eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+    }
+
+    aveToNum(avenue) {
+      return eastWest.indexOf(avenue)
+    }
+    
+    blocksTravelled() {
+        let horizontal = this.aveToNum(this.endingLocation.horizontal) - this.aveToNum(this.beginningLocation.horizontal)
+        let vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+
+        return Math.abs(horizontal) + Math.abs(vertical)
+    }
+
+    estimatedTime(peak) {
+       if (peak) {
+         return this.blocksTravelled() / 2
+       } else {
+         return this.blocksTravelled() / 3
+       }
+    }
+}
+
+    
+    
 
