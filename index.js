d27d243d58b249a833b62e1678c86fb650e42d8f
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-13 16:26:18 -0400

    Done.

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ class Driver {
 
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
-// const callback = function (el, i) {
-//   if (el === street) {
-//     return i + 1
-//   }
-// }
-
 function horizontalcalc(array, street) {
   let i = 0
   for (const element of array) {
@@ -28,15 +22,13 @@ function horizontalcalc(array, street) {
 }
 
 class Route {
-  constructor(obj1, obj2) {
-    this.horizontal1 = obj1.horizontal
-    this.vertical1 = obj1.vertical
-    this.horizontal2 = obj2.horizontal
-    this.vertical2 = obj2.vertical
+  constructor(beginning, ending) {
+    this.beginning = beginning
+    this.ending = ending
   }
   blocksTravelled() {
-    let vertical = this.vertical2 - this.vertical1
-    let horizontal = horizontalcalc(eastWest, this.horizontal2) - horizontalcalc(eastWest, this.horizontal1)
+    let vertical = this.ending.vertical - this.beginning.vertical
+    let horizontal = horizontalcalc(eastWest, this.ending.horizontal) - horizontalcalc(eastWest, this.beginning.horizontal)
     return horizontal + vertical
   }
   estimatedTime(condition = false) {
