e8dbfeb64fd62e58d873de44276acd941c43ff09
arye-dov-eidelman <aryedov.e@gmail.com>
2018-09-13 14:11:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    const isNotExact = (this.startDate.getDate() + this.startDate.getMonth() > 0)
+    return endDate - this.startDate.getFullYear() - isNotExact
+  }
+}
 
+class Route{
+  constructor(pickup, dropOff){
+    this.pickup = pickup
+    this.dropOff = dropOff
+  }
+  
+  distance(a, b){
+    if (a === b){ return 0 }
+    return Math.abs((parseInt(a) || 0) - (parseInt(b) || 0))
+  }
+
+  verticalBlocksTravelled(){
+    return this.distance(this.pickup.vertical, this.dropOff.vertical)
+  }
+  
+  horizontalBlocksTravelled(){
+    return this.distance(this.pickup.horizontal, this.dropOff.horizontal)
+  }
+
+  blocksTravelled(){
+    return this.verticalBlocksTravelled() + this.horizontalBlocksTravelled()
+  }
+
+  estimatedTime(){
+    return this.blocksTravelled() * 3
+  }
+}
