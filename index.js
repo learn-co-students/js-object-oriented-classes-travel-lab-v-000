70eebec275a8c33ec9f34a17cb8073cfe760b74f
amckean12 <amckean@me.com>
2018-10-03 14:06:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year){
+    let today = new Date()
+    let startYear = this.startDate.getYear()
+    let currentYear = today.getYear()
+    let experience = currentYear - startYear - 1 //Need to find out if there is a better way to do this or if this is right.
+    return experience
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+
+  }
+}
