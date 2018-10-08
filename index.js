b20857571e02d1c0b6e835022a0df938ad135fb5
amckean12 <amckean@me.com>
2018-10-08 13:15:08 -0500

    Done.

--- index.js
+++ index.js
@@ -21,7 +21,21 @@ class Route {
     this.endingLocation = endingLocation
   }
 
-  blocksTravelled(){
+  avenueToInteger(avenue) {
+   return eastWest.indexOf(avenue);
+ }
 
+  blocksTravelled(){
+    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
   }
 }
