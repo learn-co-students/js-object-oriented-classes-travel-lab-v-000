d1dfdec3fc801b0ebe2e6331b9a639daf78be358
roitblatari <roitblatari@gmail.com>
2018-10-09 12:58:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+]
 
+
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate =  new Date(startDate);
+    //
+  }
+  yearsExperienceFromBeginningOf(year){
+   let endDate = new Date(year, 1, 1)
+   return parseInt((endDate - this.startDate) / (1000 * 60 * 60 * 24 * 365))
+  }
+}
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  aveIndex(ave){
+    return eastWest.indexOf(ave);
+  }
+
+  blocksTravelled(){
+
+    let horizontalDistance =
+      this.aveIndex(this.endingLocation.horizontal) -
+      this.aveIndex(this.beginningLocation.horizontal);
+
+    let verticalDistance =
+      this.endingLocation.vertical -
+      this.beginningLocation.vertical ;
+
+    return Math.abs(verticalDistance)  + Math.abs(horizontalDistance);
+  }
+
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+
+}
