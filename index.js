38b8b82e8a80279c2137b817db057a5b52e54372
stazman <cadistasio@gmail.com>
2018-10-18 15:00:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+class Driver {
+    constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+    }
+    
+    yearsExperienceFromBeginningOf() {
+        const newDate = new Date(new Date().getFullYear(), 0, 1);
+        return newDate - this.startDate.getFullYear();
+    }
+}
+// let driver = new Driver('Alfie', 'Jan 1, 1995');
+
+// class Route {
+//     constructor(beginningLocation, EndLocation) {
+
+//     }
+// ]
