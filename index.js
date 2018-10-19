9fbab84753b7570a7fdd0da30f2185380cccf009
stazman <cadistasio@gmail.com>
2018-10-19 09:30:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 
 class Driver {
     constructor(name, startDate) {
@@ -6,15 +8,34 @@ class Driver {
     this.startDate = new Date(startDate);
     }
     
-    yearsExperienceFromBeginningOf() {
-        const newDate = new Date(new Date().getFullYear(), 0, 1);
-        return newDate - this.startDate.getFullYear();
+    yearsExperienceFromBeginningOf(year) {
+        const currentDate = new Date(year, 1, 1);
+        const totalYears = (currentDate - this.startDate) / (365 * 24 * 60 * 60 * 1000);
+        return parseInt(totalYears);
     }
 }
-// let driver = new Driver('Alfie', 'Jan 1, 1995');
 
-// class Route {
-//     constructor(beginningLocation, EndLocation) {
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    }
 
-//     }
-// ]
+    avenueToInteger(avenue) {
+        return eastWest.indexOf(avenue)
+    }
+
+    blocksTravelled() {
+        let horDist = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
+        let verDist = this.endingLocation.vertical - this.beginningLocation.vertical;  
+        return Math.abs(horDist) + Math.abs(verDist); 
+    }
+
+    estimatedTime(peak) {
+        if (peak) {
+            return this.blocksTravelled() / 2
+        } else {
+            return this.blocksTravelled() / 3
+        } 
+    }
+}
