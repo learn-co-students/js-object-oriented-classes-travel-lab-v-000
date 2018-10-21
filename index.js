cfeb25f3ad5910605631747ad775904915e2c958
ElleeB <laura.trager@gmail.com>
2018-10-21 14:58:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Driver {
 
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(currentYear) {
+    return currentYear - this.startDate.getFullYear()
+  }
+}
