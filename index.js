c2d6c5e33eaa28c1881a74ba2c66bbd8cbfc0ac4
asabogal <asabogal.git@gmail.com>
2018-10-25 15:07:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue',
+                'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+    yearsExperienceFromBeginningOf(endYear){
+      return endYear - this.startDate.getFullYear()
+    }
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    debugger;
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  aveBlocks(){}
+
+  blocksTravelled(){
+    let verticalBlocks =
+  }
+
+};
