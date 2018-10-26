bfc7083cb74ea6c6c7575c7ebc8fd7d0c0d9a76b
Rachel Lim <rslim87@gmail.com>
2018-10-25 21:53:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
+	constructor(name, startDate) {
+		this.name = name;
+		this.startDate = new Date(startDate)
+	}
+	// startDate(){
+	// 	return new Date
 
+	// }
+
+	yearsExperienceFromBeginningOf(year) {
+		return year - this.startDate.getFullYear();
+	}
+}
+
+class Route {
+	  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  horizontalDistance(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue','Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let endAve = eastWest.indexOf(this.endingLocation.horizontal)
+    let startAve = eastWest.indexOf(this.beginningLocation.horizontal)
+    return Math.abs(endAve - startAve)
+  }
+   verticalDistance(){
+    let endStreet = this.endingLocation.vertical
+    let startStreet = this.beginningLocation.vertical
+    return Math.abs(endStreet - startStreet)
+  }
+   blocksTravelled(){return this.horizontalDistance() + this.verticalDistance()}
+
+	estimatedTime(peak){
+		    let time
+      peak ? time = this.blocksTravelled() /2 : time = this.blocksTravelled()/3
+    return time
+
+	}
+}
