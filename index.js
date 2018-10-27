5974064633cac5604bccd951bdd7284d048fda2d
Carol Greene <carolgreene3@gmail.com>
2018-10-27 20:37:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 class Driver {
   constructor(name, startDate) {
-    this.name = name;
-    this.startDate = new Date(startDate)
+    this.name = name
+    this.startDate = new Date(startDate)     //to create a new Date object
+    //console.log(this.startDate)
   }
 
-  yearsExperienceFromBeginningOf(date) {
-    return date - this.startDate.getUTCFullYear() - 1
+  //yearsExperienceFromBeginningOf(year) {
+  //  return year - this.startDate.getUTCFullYear() - 1
+  //}
+
+  //or can do Learn solution which actually takes it back to the beginning of the year
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 0, 1)           //this will make endDate jan 1st of argument year
+    let totalYears = (endDate - this.startDate) /  //gives answer in millisecs so you have
+    (365 * 24 * 60 * 60 * 1000)                   // to divide by # millisecs per year(days * hours * min * sec * millisecs)
+    //console.log(totalYears)
+  return Math.floor(totalYears)                 //Math.floor removes the decimal & just leaves full years
   }
 }
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue',
+  'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Route {
-  constructor(beginningLocation, endingLocation) {
-    this.beginningLocation = beginningLocation;
-    this.endingLocation = endingLocation;
+  constructor(beginningLocation, endingLocation) { //both args are objects w/horizontal & vertical attributes
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
   }
 
   blocksTravelled() {
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue',
-    'Park', 'Madison Avenue', '5th Avenue']
+    let horizontalDistance = eastWest.indexOf(this.endingLocation.horizontal) -  //use index of eastWest array
+    eastWest.indexOf(this.beginningLocation.horizontal);
 
-    let horizontalDistance = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
-
-    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
-
-    return horizontalDistance + verticalDistance;
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical   //vertical can just be subtracted
+    return horizontalDistance + verticalDistance              //add horizontal & vertical distances
   }
 
-  estimatedTime(peak) {
+  estimatedTime(peak) {                     //whether or not peak time is fed in
     if (peak) {
-      return this.blocksTravelled() / 2
+      return this.blocksTravelled() / 2      //use return value from above function to get time
+    } else {
+    return this.blocksTravelled() / 3
     }
-    else {
-      return this.blocksTravelled() / 3
-
-    }
-
   }
+
 }
