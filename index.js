631c48578d9f64b514523d2a5d97acaefd015f42
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-11 15:21:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.startDate = new Date(date);
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+   return Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical) +
+   Math.abs((eastWest.indexOf(this.beginningLocation.horizontal) + 1) - (eastWest.indexOf(this.endingLocation.horizontal) + 1));
+  }
+
+  estimatedTime(time) {
+    if (time) {
+      return this.blocksTravelled() * 3;
+    } else {
+     return this.blocksTravelled() * 2;
+    }
+  }
+}
