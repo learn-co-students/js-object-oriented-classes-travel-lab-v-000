3210c3ac3305e3e4292cc7e389e6b9e7d988fc37
AAM77 <Adeel.A.Mhd@gmail.com>
2018-11-20 20:21:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
 
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  };
+}
+
+// Route Class
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+class Route {
+
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  convertAvenue(avenueName) {
+    if (eastWest.indexOf(avenueName) > -1) {
+      return eastWest.indexOf(avenueName);
+    } else {
+      return 0;
+    }
+  }
+
+  blocksTravelled() {
+    //debugger;
+    let verticalTravelled = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let horizontalTravelled = this.convertAvenue(this.endingLocation.horizontal) - this.convertAvenue(this.beginningLocation.horizontal);
+    let totalTravel = horizontalTravelled + verticalTravelled;
+
+    return totalTravel;
+  }
+
+  estimatedTime(peakHours) {
+    if (peakHours) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+
+}
