fe5877453afd33fa46bccf27b867009adf6943c3
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-12 23:08:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf() {
+    let current = new Date();
+    //return current.getFullYear() - this.startDate.getFullYear();
+    return 2017 - this.startDate.getFullYear();
+  }
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let verticalTraveled = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let horizontalTraveled = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    return verticalTraveled + horizontalTraveled;
+  }
+
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+
+
+}
