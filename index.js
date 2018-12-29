8b54afa93d8987e8f33fa7cc281b20f8ff84bf17
Jenny Kam <jkam17@gmail.com>
2018-12-28 20:31:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver{
+  constructor(name, date){
+    this.name = name
+    this.startDate = new Date(date)
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+}
+
+class Route{
+  constructor (start, end){
+    this.start = start
+    this.end = end
+  }
+
+  blocksTravelled(){
+
+    let vert = Math.abs(this.end.vertical - this.start.vertical)
+    // debugger
+    let horiz = Math.abs(
+      eastWest.indexOf(this.end.horizontal) - eastWest.indexOf(this.start.horizontal))
+    return vert + horiz
+  }
+
+  estimatedTime(peakBool){
+    if (peakBool){
+      return this.blocksTravelled() / 2
+    }
+    else {
+      return this.blocksTravelled() / 3
+    }
+  }
+
+}
