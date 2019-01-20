fc235f41c24ec38e6e116c918ce84a5f83b46785
David McGrann <dpmcgrann@gmail.com>
2019-01-20 11:37:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    let verticalDistance = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let horizontalDistance = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    return verticalDistance + horizontalDistance
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return (this.blocksTravelled() / 2);
+    } else {
+      return (this.blocksTravelled() / 3);
+    }
+  }
+}
