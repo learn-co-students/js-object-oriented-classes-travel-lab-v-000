468670e930ec2ac8d0d88a2dcdfa7f2b739f8bd6
lauradarlak <lauradarlak@gmail.com>
2019-01-23 12:20:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return new Date(year).getUTCFullYear() - this.startDate.getUTCFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue)
+  }
+
+  blocksTravelled(){
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+    let horizontalDistance =
+      this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
+    return verticalDistance + horizontalDistance;
+  }
+
+  estimatedTime(peak){
+    if (peak){
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
