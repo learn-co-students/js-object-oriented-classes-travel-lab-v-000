eb494386aaa72ce441a50dad99df5040587cd325
ConorHamilton19 <conhamil19@gmail.com>
2019-02-05 14:48:50 -0600

    Done.

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ class Route {
   }
 
   blocksTravelled () {
-    let avenues = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal);
+    let avenues = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal)
 
-    let streets = this.beginningLocation.vertical - this.endingLocation.vertical;
+    let streets = this.beginningLocation.vertical - this.endingLocation.vertical
 
-    let blocks = Math.abs(avenues) + Math.abs(streets);
-    return blocks;
+    let blocks = Math.abs(avenues) + Math.abs(streets)
+    return blocks
   }
   estimatedTime(peakHours){
-    return peakHours ? this.blocksTravelled()/2 : this.blocksTravelled()/3;
+    return peakHours ? this.blocksTravelled()/2 : this.blocksTravelled()/3
   }
 }
