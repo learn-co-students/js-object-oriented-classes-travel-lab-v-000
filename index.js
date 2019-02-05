a720ca652e1950a9396a098bf85bb52a930c8b38
JuanGongora <gongora.animations@gmail.com>
2019-02-04 19:15:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
 
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    this.year = new Date(year, 1, 1);
+    let totalYears = this.year.getFullYear() - this.startDate.getFullYear();
+    return totalYears;
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    let vertical = (this.endingLocation.vertical - this.beginningLocation.vertical);
+    return horizontal + vertical;
+  }
+
+  estimatedTime(peak) {
+    // JS ternary operator
+    return peak ? (this.blocksTravelled() / 2) : (this.blocksTravelled() / 3);
+  }
+}
