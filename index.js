05668a3332793ab3f320740dcdf31ecb8336766e
Pamela Frieze <pfrieze@gmail.com>
2019-02-12 01:53:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date (year, 1, 1); 
+    let endYear = endDate.getFullYear;
+    let startYear = this.startDate.getFullYear;
+    return endYear - startYear;
+  }
+}
