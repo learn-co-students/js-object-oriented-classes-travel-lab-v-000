2b82bffb023fc3704b99ebb7dbfba7ca05aae2f6
amandena <manda.panda91@yahoo.com>
2019-02-13 18:10:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue);
+  }
+
+  blocksTravelled() {
+    let horizontal = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
+
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+    return horizontal + vertical;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
