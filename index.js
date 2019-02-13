84520a8da9607478bd37757e326d586495c1e164
ellefaraday <faraday.elle@gmail.com>
2019-02-13 12:21:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled(){
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    const horizontalStart = this.beginningLocation.horizontal
+    const horizontalEnd = this.endingLocation.horizontal
+    const verticalStart = this.beginningLocation.vertical
+    const verticalEnd = this.endingLocation.vertical
+
+    const northSouthBlocks = Math.abs(verticalEnd - verticalStart)
+    const eastWestBlocks = Math.abs(eastWest.indexOf(horizontalEnd) - eastWest.indexOf(horizontalStart))
+    return northSouthBlocks + eastWestBlocks
+  }
+
+  estimatedTime(peak = false) {
+    let rate = 3
+    if(peak){
+      rate = 2
+    }
+    return this.blocksTravelled() / rate
+  }
+}
