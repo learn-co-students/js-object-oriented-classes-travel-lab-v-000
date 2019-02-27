60e414e463e8e943ad133e8519466c662721edc9
ryanwevans <ryanwevans@icloud.com>
2019-02-26 22:21:07 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  };
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    const verticalBlocks = this.endingLocation.vertical - this.beginningLocation.vertical;
+    const horizontalBlocks = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    return verticalBlocks + horizontalBlocks;
+  };
+
+  estimatedTime(peakTime) {
+    if (peakTime) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    };
+  };
+
+};
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
