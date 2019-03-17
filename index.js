590f744b322c10cd15ddd433a38aba4e067fe8ea
Catherine Bronzo <cbronzo@mac.com>
2019-03-17 23:30:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date( startDate )
+  }
+  
+  yearsExperienceFromBeginningOf (endDate) {
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+  this.beginningLocation = beginningLocation
+  this.endingLocation = endingLocation
+    }
+  
+  blocksTravelled () {
+  let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+  ]
+  
+  const vert = this.endingLocation.vertical - this.beginningLocation.vertical
+    const horiz_end = eastWest.indexOf(this.endingLocation.horizontal)
+    const horiz_start = eastWest.indexOf(this.beginningLocation.horizontal)
+    const horiz = horiz_end - horiz_start;
+
+     return vert + horiz;
+  }
+  
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+    
+  }
+
+}
