90857396e9e6a2a8058e7d6540ff1f656d2bb798
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-09 08:09:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return parseInt(endDate) - this.startDate.getFullYear();
+  }
+}
