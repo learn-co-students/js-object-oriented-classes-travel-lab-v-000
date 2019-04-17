653af4ba9e1bbaae2e8f1c257cb9b119913ad9fb
Rachel Wyatt <rw126@evansville.edu>
2019-04-17 18:14:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date (startDate + "Z");
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    let startYear = this.startDate.getFullYear();
+    return (year - startYear - 1);
+  }
+}
+class Route {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  blocksTravelled() {
+    const avenues = [
+      "1st Avenue",
+      "2nd Avenue",
+      "3rd Avenue",
+      "Lexington Avenue",
+      "Park",
+      "Madison Avenue",
+      "5th Avenue"
+    ];
+    const vertical = Math.abs(this.end.vertical - this.start.vertical);
+    const horizontal = Math.abs(
+      avenues.indexOf(this.end.horizontal) -
+        avenues.indexOf(this.start.horizontal)
+    );
+    return vertical + horizontal;
+  }
+  
+  estimatedTime(peak = false) {
+    if (peak === false) {
+      return this.blocksTravelled() / 3;
+    } else {
+      return this.blocksTravelled() / 2;
+    }
+  }
+}
 
