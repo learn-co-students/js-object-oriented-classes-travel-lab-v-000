70443e9517cff2c33925e66dd9c6f435f6a50dfc
harleyharl <harleymteden@gmail.com>
2019-05-09 10:35:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date( startDate );
+  };
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return (endDate - this.startDate.getFullYear());
+  };
+}
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  avenueToInteger( avenue ) {
+    return eastWest.indexOf( avenue );
+  }
+
+  blocksTravelled() {
+    return (Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)) + Math.abs(this.avenueToInteger(this.beginningLocation.horizontal) - this.avenueToInteger(this.endingLocation.horizontal))
+  }
+
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
