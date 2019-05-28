2a9cb76add27a6fddc3da92af52fac515d7624ed
sjwakean <swakeman@ca.rr.com>
2019-05-28 14:52:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+class Driver {
+    constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date( startDate );
+    }
 
+    yearsExperienceFromBeginningOf(year) {
+        return year - this.startDate.getFullYear()
+    }
+}
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+    }
+
+    blocksTravelled() {
+        let northSouth = endingLocation - beginningLocation;
+        let eastWest = [
+            '1st Avenue',
+            '2nd Avenue',
+            '3rd Avenue',
+            'Lexington Avenue',
+            'Park',
+            'Madison Avenue',
+            '5th Avenue'
+          ];
+
+          const index = beginningLocation.findIndex(horizontal => horizontal === eastWest) 
+
+          
+          if beginningLocation == eastWest || endingLocation == eastWest {
+            let start = eastWest.findIndex
+            let end = eastWest.findIndex
+                return end - start;
+          }
+          else {
+            return endingLocation - beginningLocation
+          }
+        
+          
+
+    }
+}
