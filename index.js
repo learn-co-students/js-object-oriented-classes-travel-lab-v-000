a008d3d3b89fae7dab9b2b0074ea98f6640efd4a
brennanmrose <brennan.m.rose@gmail.com>
2019-06-04 13:38:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Driver {
+	constructor(name, startDate) {
+		this.name = name;
+		this.startDate = new Date()
+	}
 
+	yearsExperienceFromBeginningOf(endDate) {
+		let endDate = new Date()
+		return endDate - this.startDate.getFullYear(), 0, 1;
+	}
+}
