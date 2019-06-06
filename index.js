1241dc50be1eac3019cfb8a334b8c354730900c8
sarawall1187 <sarawall1187@gmail.com>
2019-06-06 14:01:57 -0700

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ class Driver {
         return year - this.startDate.getFullYear();
     }
 }
-
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
 
 class Route {
     constructor(beginningLocation, endingLocation){
@@ -16,7 +24,17 @@ class Route {
         this.endingLocation = endingLocation;
     }
 
-    blocksTravelled(){
-        this.beginningLocation - this.endingLocation;
+    blocksTravelled() {
+        let verticalBlocks = this.beginningLocation.vertical - this.endingLocation.vertical;
+        let horizontalBlocks = eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal);
+        return -(verticalBlocks + horizontalBlocks);
+    }
+
+    estimatedTime(peakHours) {
+        if(peakHours) {
+          return this.blocksTravelled()/2;
+        } else {
+          return this.blocksTravelled()/3;
+        };
     }
 }
