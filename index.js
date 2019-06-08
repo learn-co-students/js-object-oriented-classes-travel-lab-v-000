e43dd39d6c7943cfa64ab9cf5be009579e55a499
lpzraf <lpz.raf@gmail.com>
2019-06-07 20:41:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+class Driver {
+  constructor(name, startDate){
+  this.name = name
+  this.startDate = new Date();
+}
+    yearsExperienceFromBeginningOf(year) {
+      return year - this.startDate  
+    }
+    blocksTravelled() {
+      return ''
+    }
+}
 
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+  this.beginningLocation = beginningLocation
+  this.endingLocation = endingLocation
+}
+
+    blocksTravelled() {
+      return ''
+    }
+    estimatedTime() {
+      return ''
+    }
+}
