cd7aebfa9aeed2a9570d3de2a98ba462d41087f1
catherinism <catcreates13@gmail.com>
2019-06-15 15:57:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+class Driver {
+  debugger
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    this.year = year
+    return this.year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  // debugger
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    const vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+    const horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+
+    return vertical + horizontal;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
