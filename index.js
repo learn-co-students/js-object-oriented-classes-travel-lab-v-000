690e48c344433cef8d8109a8592825afe27b721d
Dan Hintz <dhintz@live.com>
2019-06-24 00:17:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+  constructor(name, joinDate) {
+    this.name = name;
+    this.startDate = new Date(joinDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled() {
+    const eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    const vert = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    let begHorPos = this.beginningLocation.horizontal;
+    let endHorPos = this.endingLocation.horizontal;
+    const hor = Math.abs(eastWest.findIndex(street => street === endHorPos) - eastWest.findIndex(street => street === begHorPos));
+    return vert + hor;
+  }
+  
+  estimatedTime(peak) {
+    if (peak === true) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
 
