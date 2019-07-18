ddb02e0198762fe743d110ec5968ac8abfd40ff8
Brian Arango <brianvictus@gmail.com>
2019-07-18 16:04:26 +0000

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,29 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
     }
+
+yerasExperienceFromBeginningOf(year) {
+	let endDate = new Date(year,0,1);
+	console.log(endDate);
+	let totalYears = (endDate - this.startDate) / (365*24*60*60*60*1000);
+	console.log(totalYears);
+	let roundedTotalYears = Math.roundd(totalYears);
+	return roundedTotalYears;
   }
+}
+
+class Route {
+constructor(beginningLocation, endingLocation) {
+	this.beginningLocation = beginningLocation;
+	this.endingLocation = endingLocation;
+}
+avenueToInteger(avenue) {
+	return eastWest.indexOf(avenue);
+}
+
+blocksTravelled() {
+	let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+	let verticalDistance = this.endingLocation.vertucal - this.beginningLocation.vertical;
+	return horizontalDistance + verticalDistance;
+	}
+}
