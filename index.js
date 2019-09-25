8b544d5accabc7ca9f77a34e84ef260515e1e653
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-25 17:51:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver {
+  constructor(name, startDate){
+    
+  }
+  
+  startDate(){
+    
+  }
+  
+  yearsExperienceFromBeginningOf(){
+    
+  }
+  
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
