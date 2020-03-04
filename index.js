0ef9682e1b882b1c03ce589315c5efa7954806d9
RonSala <ron.sala@gmail.com>
2020-03-04 14:55:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 class Driver {
   constructor(name, date) {
     this.name = name;
-    this.startDate = new Date(2020, 3, 5);
-    // function () {
-      // debugger
-      // let splitDate = date.split(' ');
-      // let year = splitDate[2];
-
-      // return new Date(2020, 3, 5)
-    }
-
-    // debugger
-  // }
-
-  // dateMaker() {
-
-  // }
+    this.startDate = new Date(date);
+  }
 }
