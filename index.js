46599128cb2f97a66d3d056d18e6061ecf4539b8
RonSala <ron.sala@gmail.com>
2020-03-04 18:12:25 -0500

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,47 @@ class Driver {
     this.name = name;
     this.startDate = new Date(date);
   }
+
+  yearsExperienceFromBeginningOf(year) {
+    let start = this.startDate.getFullYear();
+    let end = year;
+    return end - start;
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+
+    let diff = function(a, b) {
+      return Math.abs(a - b);
+    }
+
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let horIndex = function (str) {
+      return eastWest.indexOf(str);
+    }
+
+    let horIndexBegin = horIndex(this.beginningLocation.horizontal);
+    let horIndexEnd = horIndex(this.endingLocation.horizontal);
+
+    let horizontalBlocks = diff(horIndexBegin, horIndexEnd);
+
+    let verticalBlocks = diff(this.beginningLocation.vertical, this.endingLocation.vertical);
+
+    return horizontalBlocks + verticalBlocks;
+  }
 }
